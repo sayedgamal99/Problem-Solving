c7c117d84c704385cd063ffeb19f30960d6f6502
sayedgamal99 <sayyedgamall@gmail.com>
2024-02-23 15:28:34 +0200

    math(Summation) simple solution added

--- Python/LeetCode/zDailyChallenge/268MissingNumber.py
+++ Python/LeetCode/zDailyChallenge/268MissingNumber.py
@@ -1,3 +1,4 @@
+# solution one: manipulating with values:
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
         n = len(nums)
@@ -7,4 +8,15 @@ class Solution:
         return nums.index(max(nums))
 
 
-print(Solution().missingNumber(nums=[8, 6, 4, 2, 3, 5, 7, 1]))
+# print(Solution().missingNumber(nums=[8, 6, 4, 2, 3, 5, 7, 1]))
+
+
+# Solution 2 with math using summation:
+
+class Solution2:
+    def missingNumber(self, nums: list[int]) -> int:
+        summ = len(nums)*(len(nums)+1)//2
+        return summ - sum(nums)
+
+
+print(Solution2().missingNumber(nums=[8, 6, 4, 2, 3, 5, 7, 1]))
