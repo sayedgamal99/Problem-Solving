dde59a354e78d41082efe7dacfa83d1407e717db
sayedgamal99 <sayyedgamall@gmail.com>
2024-03-04 21:06:59 +0200

    recursive solution for inorder traversel tree, call left, add value, call right

---
+++ Python/LeetCode/Trees/94BinaryTreeInorderTraversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# iterative Solution using stack:
+
+# recursive solution:
+class Solution:
+    def inorderTraversal(self, root) -> list[int]:
+        res = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        return res
