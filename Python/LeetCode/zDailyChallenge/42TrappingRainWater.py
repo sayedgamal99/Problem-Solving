a80d54735ade2bb6df3df25e8366f9071134ad89
sayedgamal99 <sayyedgamall@gmail.com>
2024-04-12 15:25:07 +0200

    another solution with extra memory using max left bar and max right bar at each position

--- Python/LeetCode/zDailyChallenge/42TrappingRainWater.py
+++ Python/LeetCode/zDailyChallenge/42TrappingRainWater.py
@@ -1,10 +1,29 @@
+
+# two pointers solution
+class Solution2:
+    def trap(self, height: list[int]) -> int:
+        maxleft = [0]
+        for i in range(1, len(height)):
+            maxleft.append(max(maxleft[-1], height[i-1]))
+        maxright = [0]
+        for i in range(len(height)-2, -1, -1):
+            maxright.append(max(maxright[-1], height[i+1]))
+        maxright.reverse()
+        result = 0
+        for i, h in enumerate(height):
+            T = (min(maxleft[i], maxright[i])-h)
+            result += T if T > 0 else 0
+        return result
+
+
+# monotonic decreasing stack
 class Solution:
     def trap(self, height: list[int]) -> int:
         stack = []  # decreasing stack
         res = 0
         for i, r in enumerate(height):
             while stack and r > stack[-1][0]:
-                bar, j = stack.pop()
+                bar, _ = stack.pop()
                 if stack:
                     l, leftBound = stack[-1][1], stack[-1][0]
                     minBound = min(r, leftBound)
