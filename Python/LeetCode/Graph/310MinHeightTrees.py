74ec35667b74b72861f86a30e1083ee7622dcf92
sayedgamal99 <sayyedgamall@gmail.com>
2024-04-23 18:13:56 +0200

    TLE Valid Solution with Graph,BFS (iterative brute force)

---
+++ Python/LeetCode/Graph/310MinHeightTrees.py
@@ -0,0 +1,37 @@
+
+# Valid Solution with Graph,BFS (iterative brute force)
+from collections import defaultdict, deque
+
+
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: list[list[int]]) -> list[int]:
+        thegraph = self.edgesToGraph(edges)
+        MHT = defaultdict(list)
+        for t in thegraph:
+
+            q = deque()
+            q.append([t, 0])
+            visited = set()
+            visited.add(t)
+
+            while q:
+                root, height = q.popleft()
+                for child in thegraph[root]:
+                    if child not in visited:
+                        visited.add(child)
+                        q.append([child, height+1])
+            MHT[height].append(t)
+
+        return MHT[min(MHT)]
+
+    def edgesToGraph(self, edges):
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        return graph
+
+
+# print(Solution().findMinHeightTrees(4, [[1, 0], [1, 2], [1, 3]]))
+# print(Solution().findMinHeightTrees(
+#     6, [[3, 0], [3, 1], [3, 2], [3, 4], [5, 4]]))
