94db830987d6657ad2e5f0d047b6ab78ece053b4
sayedgamal99 <sayyedgamall@gmail.com>
2024-04-23 19:15:07 +0200

    using the idea of the result will be always 1-2 nodes running bfs to eliminates leave nodes is the solution

--- Python/LeetCode/Graph/310MinHeightTrees.py
+++ Python/LeetCode/Graph/310MinHeightTrees.py
@@ -1,9 +1,45 @@
-
-# Valid Solution with Graph,BFS (iterative brute force)
 from collections import defaultdict, deque
 
 
 class Solution:
+    def findMinHeightTrees(self, n: int, edges: list[list[int]]) -> list[int]:
+        if n == 1:
+            return [0]
+
+        thegraph = defaultdict(list)
+        for u, v in edges:
+            thegraph[u].append(v)
+            thegraph[v].append(u)
+
+        edge_cnt = {}
+        leaves = deque()
+
+        for root, neighbors in thegraph.items():
+            if len(neighbors) == 1:
+                leaves.append(root)
+            edge_cnt[root] = len(neighbors)
+
+        while leaves:
+
+            if n <= 2:
+                return list(leaves)
+
+            for _ in range(len(leaves)):
+                node = leaves.popleft()
+
+                n -= 1
+                for nei in thegraph[node]:
+                    edge_cnt[nei] -= 1
+                    if edge_cnt[nei] == 1:
+                        leaves.append(nei)
+
+
+print(Solution().findMinHeightTrees(4, [[1, 0], [1, 2], [1, 3]]))
+print(Solution().findMinHeightTrees(
+    6, [[3, 0], [3, 1], [3, 2], [3, 4], [5, 4]]))
+
+
+class SolutionTLE:
     def findMinHeightTrees(self, n: int, edges: list[list[int]]) -> list[int]:
         thegraph = self.edgesToGraph(edges)
         MHT = defaultdict(list)
@@ -32,6 +68,6 @@ class Solution:
         return graph
 
 
-# print(Solution().findMinHeightTrees(4, [[1, 0], [1, 2], [1, 3]]))
-# print(Solution().findMinHeightTrees(
+# print(SolutionTLE().findMinHeightTrees(4, [[1, 0], [1, 2], [1, 3]]))
+# print(SolutionTLE().findMinHeightTrees(
 #     6, [[3, 0], [3, 1], [3, 2], [3, 4], [5, 4]]))
